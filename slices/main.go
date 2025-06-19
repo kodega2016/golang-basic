2f5f3ba5f39b66cc6a53eba82f62014fd8e7e87f
Khadga Shrestha <khadgalovecoding2016@gmail.com>
2025-06-19 07:37:57 +0545

    added slice with make

--- slices/main.go
+++ slices/main.go
@@ -6,4 +6,8 @@ func main() {
 	users := []string{}
 	users = append(users, "sakar", "himal")
 	fmt.Println(users)
+
+	skills := make([]string, 3)
+	skills = append(skills, "docker", "aws")
+	fmt.Println("skills:", skills)
 }
