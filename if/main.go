ce8237a8bcc3a8b225f07ff469e3f61bb143c732
Khadga Shrestha <khadgalovecoding2016@gmail.com>
2025-06-19 07:18:57 +0545

    added if statement

---
+++ if/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 20
+
+	if age < 20 {
+		fmt.Println("you are not allowed to enter this site")
+	} else {
+		fmt.Println("you are welcome...")
+	}
+}
